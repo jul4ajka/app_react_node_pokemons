c52cfb4dab0ac443fb89b2373222985b6d214fae
jul4ajka <jul4ajka@gmail.com>
2020-11-04 22:31:53 +0200

    res

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ app.use(bodyParser.urlencoded({
 }))
 
 //Усі адреси регулюються з index.html
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/public/build/index.html');
-});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
