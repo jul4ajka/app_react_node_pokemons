382b569cbf6038861424f49b33e54e642ecfcf58
jul4ajka <jul4ajka@gmail.com>
2020-11-06 16:15:43 +0200

    Updates

---
+++ precache-manifest.c9c3f09338e78e024a959d8daf0a1fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d6066cd19e12eb577b07c13dfcfdf1",
+    "url": "/pokemons_react_node_app/index.html"
+  },
+  {
+    "revision": "4df2476b9e63a1ef5e61",
+    "url": "/pokemons_react_node_app/static/css/main.85551091.chunk.css"
+  },
+  {
+    "revision": "a4abc9cf7196ef33e031",
+    "url": "/pokemons_react_node_app/static/js/2.50375577.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemons_react_node_app/static/js/2.50375577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df2476b9e63a1ef5e61",
+    "url": "/pokemons_react_node_app/static/js/main.ef4ad183.chunk.js"
+  },
+  {
+    "revision": "2310be93aa77afca51a6",
+    "url": "/pokemons_react_node_app/static/js/runtime-main.5faca9a2.js"
+  }
+]);
