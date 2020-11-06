9db0743ade42cb196bd33ed0f83cffc0bbd16ef6
jul4ajka <jul4ajka@gmail.com>
2020-11-06 16:12:59 +0200

    Updates

---
+++ precache-manifest.6563e97dd0812031891ea2f4e93afbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57413732ce7640c2ee21c893d654032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0103427c40e6676f1937",
+    "url": "/static/css/main.85551091.chunk.css"
+  },
+  {
+    "revision": "a4abc9cf7196ef33e031",
+    "url": "/static/js/2.50375577.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50375577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0103427c40e6676f1937",
+    "url": "/static/js/main.65bae0d5.chunk.js"
+  },
+  {
+    "revision": "e2fee617b0cce52d38a2",
+    "url": "/static/js/runtime-main.fde3a231.js"
+  }
+]);
